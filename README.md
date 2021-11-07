d9e8cf9547042a8fa2d1454f7ecd8811588e8530
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2021-11-07 17:07:35 -0500

    final try with password gen

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 <!-- PASSWORD GENERATOR -->
+
+
+
+
+
+
+
+
+
+var numberGenerator = function (x) {
+    var answer = window.prompt("What number");
+
+    if (!isNaN(answer)) {
+        console.log("I am the " + answer + "number that I pass")
+
+    } else {
+        console.log("Please enter a number")
+    }
+}
+numberGenerator(4)
--- assets/script.js
+++ assets/script.js
@@ -2,40 +2,68 @@
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var capLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//var specialCharacters = [/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/];
-/*
-var doYouWantNumbers = window.prompt("Do you want to use numbers in your password? Type Yes or No. ");
-var doYouWantCapitalLetters = window.prompt("Do you want to use capital letters in your password? Type Yes or No.")
-var doYouWantLowerCaseLetters = window.prompt("Do you want to use captial letters in your password? Type Yes or No.")
-var howLong = window.prompt("How many characters long do you want your password to be?");
-*/
+//var specialCharacters = [! @#$%^&*()_+-=[\]{};:\\|,.<>\/?]+/];
 
-const random = (length = 8) => {
-    return Math.random().toString(16).substr(2, length);
-};
+function userInitPrompt() {
+    var howLong = parseInt(prompt("How many characters long do you want your password to be?"))
+    if (isNaN(howLong)) {
+        alert("Use a number dummy")
+        return;
+    }
+    if (howLong < 8) {
+        alert("Pick a number greater than 8");
+        return null;
+    }
+    if (howLong > 128) {
+        alert("Pick a number less that 128");
+        return null;
+    }
 
-console.log(random()); // bb325d9f
-console.log(random(6)); // e51d83
-console.log(random(10)); // e84c416cc7
-console.log(random(14)); // ee16dfc68e361
+    var doYouWantNumbers = confirm("Do you want to use numbers in your password? Type Yes or No. ")
+    var doYouWantCapitalLetters = confirm("Do you want to use capital letters in your password? Type Yes or No.")
+    var doYouWantLowerCaseLetters = confirm("Do you want to use captial letters in your password? Type Yes or No.")
 
-var random2 = (length = 8) => {
-    return Math.random
+    if (doYouWantCapitalLetters === false && doYouWantLowerCaseLetters === false && doYouWantNumbers === false) {
+        alert("Pick at least one Character Type")
+        return null
+    }
+    var userPrompts = {
+        passLength: howLong,
+        numbers: doYouWantNumbers,
+        capital: doYouWantCapitalLetters,
+        lowercase: doYouWantCapitalLetters
+    }
+    return userPrompts;
+}
+function randomElement(array) {
+    var randoI = Math.floor(Math.random() * array.length);
+    var randoEl = array[randoI]
+    return randoEl;
 }
 
-var numberGenerator = function (x) {
-    var answer = window.prompt("What number");
-    var isType = function () {
+function generatePassword() {
+    var userPromptsObj = userInitPrompt();
+    console.log(userPromptsObj)
+    var array1 = []
+    var array2 = []
+    if (userPromptsObj.capital) {
+        array1 = array1.concat(capLetter);
+    }
+    if (userPromptsObj.lowercase) {
+        array1 = array1.concat(lowLetter);
 
     }
-    if (answer == ) {
-        console.log("I am the " + answer + "number that I pass")
-    } else {
-        console.log("Please enter a number")
+    if (userPromptsObj.numbers) {
+        array1 = array1.concat(numbers)
     }
+    var element = randomElement(array1)
+    console.log(element)
+    for (let i = 0; i < userPromptsObj.passLength; i++) {
+        var character = randomElement(array1)
+        array2.push(character)
+    }
+    return array2.join("")
 }
-numberGenerator(4)
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
